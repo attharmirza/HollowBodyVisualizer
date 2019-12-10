6c3d180cbfa3700aec72c9d38cf8de8b1e974606
attharmirza <atthar.mirza@gmail.com>
2019-12-09 23:34:15 -0500

    Added basic interactivity and audio visualization
    I have some rudimentary code in there that allows me to play pause the music with the DOM.
    
    I also used ThreeJS's audio playback and analysis tools to drive the scale of the red cube. This is not going to be the final animation, but I have the framework and data to drive all sorts of 3D objects with the audio now.

--- scripts/visualizer.js
+++ scripts/visualizer.js
@@ -1,16 +1,31 @@
 // Set up the scene and renderer
 
 var scene = new THREE.Scene();
-var sceneWidth = $('.animation').width();
-var sceneHeight = $('.animation').height();
+var sceneElem = $('.animation');
+var sceneWidth = sceneElem.width();
+var sceneHeight = sceneElem.height();
 var camera = new THREE.PerspectiveCamera(75, sceneWidth/sceneHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(sceneWidth, sceneHeight);
-$('.animation').append(renderer.domElement);
+sceneElem.append(renderer.domElement);
 
+// Bring in audio
 
-// Start adding stuff into it
+var listener = new THREE.AudioListener();
+camera.add(listener);
+
+var music = new THREE.Audio(listener);
+
+var musicLoader = new THREE.AudioLoader();
+
+musicLoader.load('/audio/music.mp3', function (buffer) {
+  music.setBuffer(buffer);
+  music.setLoop(true);
+  music.setVolume(1);
+});
+
+// Create geometry
 
 var cube = new THREE.BoxGeometry(1,1,1);
 var material = new THREE.MeshBasicMaterial({color: 0xff3333});
@@ -20,9 +35,34 @@ scene.add(DaCube);
 
 DaCube.position.z = -5;
 
+// Use the audio and geometry to drive some animation
+
+var fftSize = 32;
+var analyser = new THREE.AudioAnalyser( music, fftSize);
+
+// Render this dope stuff out, my dude
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
+
+  var musicScale = analyser.getAverageFrequency()/100 + 1;
+
+  DaCube.scale.setScalar(musicScale);
+
+  // DaCube.scale.x = musicScale;
+  // DaCube.scale.y = musicScale;
+  // DaCube.scale.z = musicScale;
 };
 
 animate();
+
+// Add interactivty
+
+sceneElem.click(function() {
+  if (music.isPlaying) {
+    music.pause();
+  } else {
+    music.play();
+  }
+});

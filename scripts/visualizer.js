a18a69a0be8a3de4b98db542d5757c81162d927b
attharmirza <atthar.mirza@gmail.com>
2019-12-15 20:13:06 -0500

    Visualizer Responds to Mouse Position
    Added code to track mouse position over ThreeJS container and have the model react accordingly. Next up is building a cool model that takes advantage of this.

--- scripts/visualizer.js
+++ scripts/visualizer.js
@@ -4,9 +4,9 @@ var scene = new THREE.Scene();
 var sceneElem = $('.animation');
 var sceneWidth = sceneElem.width();
 var sceneHeight = sceneElem.height();
-var camera = new THREE.PerspectiveCamera(75, sceneWidth/sceneHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(50, sceneWidth/sceneHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer({alpha: true});
+var renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
 renderer.setSize(sceneWidth, sceneHeight);
 sceneElem.append(renderer.domElement);
 
@@ -30,15 +30,40 @@ musicLoader.load('/audio/music.mp3', function (buffer) {
 var cube = new THREE.BoxGeometry(1,1,1);
 var material = new THREE.MeshBasicMaterial({color: 0xff3333});
 
-var DaCube = new THREE.Mesh(cube, material);
-scene.add(DaCube);
+var model = new THREE.Mesh(cube, material);
+scene.add(model);
 
-DaCube.position.z = -5;
+model.position.z = -6;
 
 // Use the audio and geometry to drive some animation
 
 var fftSize = 32;
 var analyser = new THREE.AudioAnalyser( music, fftSize);
+
+// Add interactivity
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var plane = new THREE.Plane(new THREE.Vector3(0,0,1), 1);
+var intersectPoint = new THREE.Vector3();
+
+$('.animation').mousemove(function() {
+  mouse.x = (event.clientX/sceneWidth)*2 - 1;
+  mouse.y = -(event.clientY/sceneHeight)*2 + 1;
+  console.log(mouse.x + ", " + mouse.y);
+
+  raycaster.setFromCamera(mouse, camera);
+  raycaster.ray.intersectPlane(plane, intersectPoint);
+  model.lookAt(intersectPoint);
+});
+
+sceneElem.click(function() {
+  if (music.isPlaying) {
+    music.pause();
+  } else {
+    music.play();
+  }
+});
 
 // Render this dope stuff out, my dude
 
@@ -49,21 +74,11 @@ function animate() {
   var musicRelScale = 1 + analyser.getAverageFrequency()/255;
   var musicAbsScale = analyser.getAverageFrequency()/255;
 
-  DaCube.scale.setScalar(musicRelScale);
+  model.scale.setScalar(musicRelScale);
 
-  // DaCube.scale.x = musicScale;
-  // DaCube.scale.y = musicScale;
-  // DaCube.scale.z = musicScale;
+  // model.scale.x = musicScale;
+  // model.scale.y = musicScale;
+  // model.scale.z = musicScale;
 };
 
 animate();
-
-// Add interactivty
-
-sceneElem.click(function() {
-  if (music.isPlaying) {
-    music.pause();
-  } else {
-    music.play();
-  }
-});
